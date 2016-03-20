ba748418d8bdd842ab29cf4182f0b9f869708eda
JustOneLastDance <xss186@126.com>
2016-03-20 11:32:49 +0800

    初始化项目

---
+++ NetEaseNews/NetEaseNews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NetEaseNews
+//
+//  Created by  justinchou on 16/3/20.
+//  Copyright © 2016年  justinchou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
